f6cc41dd9614355857f42903d315610ad972c54a
hackerrank <nirlesh@hackerrank.com>
2023-11-22 01:19:03 +0530

    SFA-004 | added chunks and admin

--- file/admin.py
+++ file/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from file.models import *
+
+
+#
+class FileAdmin(admin.ModelAdmin):
+    search_fields = ("directory",)
+    list_display = ("directory", "file")
+
+
+admin.site.register(Files, FileAdmin)
