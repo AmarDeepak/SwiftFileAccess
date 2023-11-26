737cfd3715fbf8223418e294d085bf4d27a431d6
Amar Deepak <amar.deepak1@globallogic.com>
2023-11-26 01:59:48 -0800

    drive upload feature added

--- file/models.py
+++ file/models.py
@@ -2,8 +2,10 @@ from django.db import models
 import uuid
 from common.models import BaseTimestampedModel
 import os
+from allauth.socialaccount.models import SocialToken, SocialApp, SocialAccount
 # Create your models here.
 from django.conf import settings
+from django.contrib.auth.models import User
 class Directory(BaseTimestampedModel):
     uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
 
@@ -12,4 +14,6 @@ def get_upload_path(instance, filename):
 
 class Files(BaseTimestampedModel):
     directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_upload_path)
+    file = models.FileField(upload_to=get_upload_path, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file_id = models.CharField(max_length=200)
--- file/views.py
+++ file/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 # Create your views here.
 from file.serializers import FileListSerializer
 from rest_framework.parsers import MultiPartParser
+from file.utils import upload_to_gdrive, get_directory
+from file.models import Files
+from allauth.socialaccount.models import SocialAccount
 
 def upload(request):
     return render(request, 'file/upload.html')
@@ -15,12 +18,35 @@ class HandleFileUpload(APIView):
     parser_classes = [MultiPartParser]
     def post(self, request):
         try:
-            data = request.data
-            serializer = FileListSerializer(data=data)
-
-            if serializer.is_valid():
-                serializer.save()
-                # print(serializer)
+            user_social_account = SocialAccount.objects.filter(user=request.user)
+            if user_social_account:
+                import pdb;pdb.set_trace()
+                user_social_account = user_social_account[0]
+                if user_social_account.provider == 'Google': # Need to remove static text Google and add the djangoChoice for Google
+                    directory = get_directory(provider = 'Google')
+                    file_ids = upload_to_gdrive(request)
+                    for file_id in file_ids:
+                        Files.objects.create(
+                            directory = directory,
+                            user = user_social_account.user,
+                            file_id = file_id,
+                        )
+                    return Response({
+                        'status': 200,
+                        'message': 'File Uploaded successfully',
+                        'data': file_ids,
+                    })
+                elif user_social_account.provider == 'Apple':
+                    pass
+                    # TODO: funtionality need to be added.
+            else:
+                # if user doesn't belong to Apple/Google provider, upload the file to local.
+                data = request.data
+                serializer = FileListSerializer(data=data)
+                # import pdb;pdb.set_trace()
+                if serializer.is_valid():
+                    serializer.save()
+                    print(serializer)
                 return Response({
                     'status': 200,
                     'message': 'File Uploaded successfully',
@@ -29,7 +55,7 @@ class HandleFileUpload(APIView):
             return Response({
                 'status': 400,
                 'message': 'File Not Uploaded successfully',
-                'data': serializer.errors
+                'data': request
             })
         except Exception as ex:
             """ EXCPETION HANDLING PENDING"""
